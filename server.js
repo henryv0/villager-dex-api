076b68ef558e7ce518ef557cfca1c3c1fe9e73a5
HENRY VO <HENRYVO94@GMAIL.com>
2020-04-20 15:20:28 +1000

    Add caching with redis
    - Use redis and create a middleware function so that other endpoints can use it if necessary

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const helmet = require('helmet');
 const fetch = require('node-fetch');
+const redis = require('redis');
 
+const client = redis.createClient();
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
@@ -17,6 +19,25 @@ const API_OPTIONS = {
   },
 };
 
+const fetchNameFromCache = (req, res, next) => {
+  const { name } = req.params;
+
+  client.get(name, (err, data) => {
+    if (err) {
+      throw new Error(err);
+    }
+
+    if (data !== null) {
+      res.json({
+        source: 'cache',
+        data: JSON.parse(data),
+      });
+    } else {
+      next();
+    }
+  });
+};
+
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
@@ -27,16 +48,26 @@ app.get('/api', (req, res) => {
   });
 });
 
-app.get('/api/villagers/:name', (req, res) => {
-  let {name} = req.params;
+app.get('/api/villagers/:name', fetchNameFromCache, (req, res) => {
+  let { name } = req.params;
   let url = `${BASE_URL}/${name}/`;
+
+  console.log('fetching from 3rd party API');
 
   fetch(url, API_OPTIONS)
     .then((res) => res.json())
     .then((json) => {
-      if (json.hasOwnProperty('error')) { throw new Error(json.error) }
+      if (json.hasOwnProperty('error')) {
+        throw new Error(json.error);
+      }
 
-      res.status(200).send(json);
+      client.set(name, JSON.stringify(json), redis.print);
+      console.log('Saved to redis: ' + name);
+
+      res.status(200).json({
+        source: 'api',
+        data: json,
+      });
     })
     .catch((err) => {
       console.error(err);
